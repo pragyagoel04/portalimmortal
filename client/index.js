33dee1a2b5ea28668f80a0ca643c661d50d7410a
Pragya Goel <pragya_goel@apple.com>
2019-06-25 02:56:33 -0700

    api calls on process

--- client/index.js
+++ client/index.js
@@ -4,15 +4,29 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './components/App';
 import Greetings from './components/Greetings';
 import SignupPage from './components/signup/SignupPage';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
 import routes from './routes';
 
-render((<Router> 
-  <App>
-    <Switch>
-    {/* <Route path="/" component={App} /> */}
-    <Route exact path="/" component={Greetings} />
-    <Route path="/signup" component={SignupPage} />
-    </Switch>
-  </App>
-</Router>),
+const store = createStore(
+  (state = {}) => state,
+  // dispatch asynchronous actions
+  applyMiddleware(thunk)
+)
+
+render((
+<Provider store={store}>
+  <Router> 
+    <App>
+      <Switch>
+      {/* <Route path="/" component={App} /> */}
+      <Route exact path="/" component={Greetings} />
+      <Route path="/signup" component={SignupPage} />
+      </Switch>
+    </App>
+  </Router>
+</Provider>
+),
 document.getElementById('app'));
